01e4ea54d181a12edfbad49236bf522f0b4e6bd5
Mikhail Makarov <vallome@yandex.ru>
2024-12-12 07:00:01 +0300

    add code for model response

--- bot/main.py
+++ bot/main.py
@@ -5,16 +5,33 @@ from aiogram import (
 ) 
 from aiogram.filters.command import Command
 import asyncio
+import aiohttp
 
 from config import *
 
 bot = Bot(api_key)
 dp = Dispatcher()
 
+url = "http://127.0.0.1:800/generate_text"
+
+async def post_request(url, data):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=data) as response:
+            return await response.json()
+
 @dp.message(Command("start"))
 async def start(message: types.Message):
     await message.reply("Начнем, пожалуй!")
-
+ 
+@dp.message()
+async def answer(message: types.Message):
+    data = {
+        "prompt": message.text
+    }
+    answer = asyncio.create_task(post_request, data)
+    response_json = await answer
+    answer_text = answer.get("text")
+    await message.reply(answer_text)
 async def main():
     await dp.start_polling(bot)
 
